2aad1a0dfc92db64ce30284755566b0f86707fb6
csae8092 <peter.andorfer@oeaw.ac.at>
2022-12-19 11:21:50 +0100

    fetch indices from emt-entities

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,12 +1,21 @@
 # bin/bash
 
+echo "fetching transkriptions from emt-transkribus-export"
 rm -rf data/editions
 wget https://github.com/emt-project/emt-transkribus-export/archive/refs/heads/main.zip
 unzip main
-
 mv ./emt-transkribus-export-main/data/editions ./data/editions
 rm main.zip
 rm -rf ./emt-transkribus-export-main
+
+echo "fetching indices from emt-entities"
+rm -rf data/indices
+
+wget https://github.com/emt-project/emt-entities/archive/refs/heads/main.zip
+unzip main
+mv ./emt-entities-main/indices ./data/indices
+rm main.zip
+rm -rf ./emt-entities-main
 
 echo "update imprint"
 ./dl_imprint.sh
