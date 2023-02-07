1903e8831e7ada4707db4ec612f902bab5d548e6
csae8092 <peter.andorfer@oeaw.ac.at>
2023-02-07 22:17:54 +0100

    cosmetics #2

--- delete_faulty_files.py
+++ delete_faulty_files.py
@@ -4,8 +4,12 @@ import csv
 from acdh_tei_pyutils.tei import TeiReader
 from tqdm import tqdm
 
+
+faulty_file_name = "./html/faulty.csv"
 faulty = []
-files = sorted(glob.glob('./data/*/*.xml'))
+files = sorted(glob.glob("./data/*/*.xml"))
+print(f"check {len(files)} files and delete not well formed ones")
+
 for x in tqdm(files, total=len(files)):
     _, tail = os.path.split(x)
     try:
@@ -14,8 +18,13 @@ for x in tqdm(files, total=len(files)):
         faulty.append([tail, e])
         os.remove(x)
 
-with open('./html/faulty.csv', 'w', newline='') as csvfile:
-    my_writer = csv.writer(csvfile, delimiter=',')
-    my_writer.writerow(['path', 'error'])
+if len(faulty) > 0:
+    print(f"writing report to '{faulty_file_name}'")
+else:
+    print(f"no faulty files found, good job!!!!")
+    faulty.append(["no faulty files found", "good job!"])
+with open(faulty_file_name, "w", newline="") as csvfile:
+    my_writer = csv.writer(csvfile, delimiter=",")
+    my_writer.writerow(["path", "error"])
     for x in faulty:
         my_writer.writerow([x[0], x[1]])
