81a92ea15c5345205dda3932330a1da0e58fc53a
Lev Z Király <LeventeZoltan.Kiraly@oeaw.ac.at>
2023-02-20 13:55:34 +0100

    Change wget to curl in sh scripts

--- dl_fundament.sh
+++ dl_fundament.sh
@@ -3,6 +3,6 @@
 echo "download and copy fundament"
 FUNDAMENT_VERSION=1.2.1
 FUNDAMENT_GIT=https://github.com/acdh-oeaw/fundament/archive/refs/tags/
-wget ${FUNDAMENT_GIT}v${FUNDAMENT_VERSION}.zip && unzip v${FUNDAMENT_VERSION}.zip && rm -rf v${FUNDAMENT_VERSION}.zip
+curl -LO ${FUNDAMENT_GIT}v${FUNDAMENT_VERSION}.zip && unzip v${FUNDAMENT_VERSION}.zip && rm -rf v${FUNDAMENT_VERSION}.zip
 cp -r fundament-${FUNDAMENT_VERSION}/dist  html/
 rm -rf fundament-${FUNDAMENT_VERSION}
--- fetch_data.sh
+++ fetch_data.sh
@@ -2,7 +2,7 @@
 
 echo "fetching transkriptions from emt-working-data"
 rm -rf data/editions && mkdir data/editions
-wget https://github.com/emt-project/emt-working-data/archive/refs/heads/main.zip
+curl -LO https://github.com/emt-project/emt-working-data/archive/refs/heads/main.zip
 unzip main
 mv -t ./data/editions/ ./emt-working-data-main/data/work-in-progress/*/*.xml
 rm main.zip
@@ -11,7 +11,7 @@ rm -rf ./emt-working-data-main
 echo "fetching indices from emt-entities"
 rm -rf data/indices
 
-wget https://github.com/emt-project/emt-entities/archive/refs/heads/main.zip
+curl -LO https://github.com/emt-project/emt-entities/archive/refs/heads/main.zip
 unzip main
 mv ./emt-entities-main/indices ./data/indices
 rm main.zip
