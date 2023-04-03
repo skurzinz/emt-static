6b0985749abfd39275e12f2019d91b82f75e1407
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-04-03 10:26:00 +0200

    minor refactoring: comments and orgs

--- html/js/osd_single.js
+++ html/js/osd_single.js
@@ -1,6 +1,7 @@
-/* function trigger when static html image was loaded */
+/* function trigger when static html img was loaded */
 
 function loadImage(container_id) {
+
     /* get container holding the image and set height and padding */
     var container = document.getElementById(container_id);
     container.style.height = "700px";
@@ -20,13 +21,10 @@ function loadImage(container_id) {
         defaultZoomLevel: 0,
         fitHorizontally: true,
         tileSources: imageURL,
-        // Initial rotation angle
-        degrees: 0,
-        // Show rotation buttons
-        showRotationControl: true,
-        // Enable touch rotation on tactile devices
+        degrees: 0, // Initial rotation angle
+        showRotationControl: true, // Show rotation buttons
         gestureSettingsTouch: {
-            pinchRotate: true
+            pinchRotate: true // Enable touch rotation on tactile devices
         }
     });
     /* remove static html image element */
@@ -44,21 +42,40 @@ function loadImage(container_id) {
 setTimeout(createOverlays, 1000);
 
 function createOverlays() {
+
     /* attach click event on linenumbers */
     var linenumbers = document.getElementsByClassName("linenumbers");
     [].forEach.call(linenumbers, function(opt) {
         opt.addEventListener("click", function(event) {
             event.preventDefault();
-            /* create overlays array based on clicked linenumbers attribute zone */
-            var overlays = [];
-            var zone = opt.getAttribute("zone");
+
+            /* 
+                since more than one viewer is open get a viewer id
+                and open relevant viewer
+            */
             var opt_id = opt.getAttribute("id").replace("#", "");
-            /* since more than one viewer is open get a viewer id and open relevant viewer */
             var viewerID = `${opt_id.split("_")[0]}_${opt_id.split("_")[1]}`;
             var viewerID = viewerID.replace("facs_", "os-id-");
             var openViewer = OpenSeadragon.getViewer(viewerID);
+
+            /*
+                size attribute contains the width,height of facsimile
+            */
             var wh = opt.getAttribute("size").split(",");
+
+             /* 
+                create overlays array based on clicked linenumbers
+                attribute zone
+            */
+            var overlays = [];
+            var zone = opt.getAttribute("zone");
+            
             if (zone !== null) {
+
+                /* 
+                    create list split by whitespace and
+                    pair every two elements
+                 */
                 var xys = zone.split(" ");
                 for (let i of xys) {
                     var xy = i.split(",");
@@ -68,30 +85,41 @@ function createOverlays() {
                             id: id,
                             x: parseInt(xy[0]),
                             y: parseInt(xy[1]),
-                            className: 'highlight'
+                            className: 'overlay'
                         }
                     )
                 }
             }
+
             /* 
                 check if linenumbers is active or not
                 if active: remove class and remove overlays
                 else: add class and add overlays
             */
             if (opt.classList.contains("active")) {
+
+                /* remove active state from linenumbers click event */
                 opt.classList.remove("active");
+
+                /* remove overlay div's */
                 for (let el of overlays) {
                     var el_id = el.id;
-                    /* remove overlay div's */
+                    
                     openViewer.removeOverlay(el_id);
                 }
 
             } else {
+
+                /* add active state for linenumbers click event */
                 opt.classList.add("active");
+
+                /* create overlay, turn pixel to viewport and add overlays */
                 for (let el of overlays) {
-                    /* get lrx and lry from overlays array */
+
+                    /* get lrx and lry from overlays list */
                     let lowerRightX = el.x;
                     let lowerRightY = el.y;
+
                     /* create div element as overlay */
                     let overlayElement = document.createElement("div");
                     let idOfOverlay = document.createAttribute("id");
@@ -100,13 +128,18 @@ function createOverlays() {
                     classOfOverlay.value = el.className;
                     overlayElement.setAttributeNode(idOfOverlay);
                     overlayElement.setAttributeNode(classOfOverlay);
+
                     /* parse xry and xrx as integer and convert to viewport coords */
                     let widthOfPicture = parseInt(wh[0]);
                     let heightOfPicture = parseInt(wh[1]);
                     let lowerRightXScaled = lowerRightX / widthOfPicture;
                     let lowerRightYScaled = lowerRightY / heightOfPicture * heightOfPicture/widthOfPicture;
+
                     /* add div el on pointLayer coords */
-                    openViewer.addOverlay(overlayElement, new OpenSeadragon.Point(lowerRightXScaled, lowerRightYScaled));
+                    openViewer.addOverlay(overlayElement, 
+                                          new OpenSeadragon.Point(
+                                                                  lowerRightXScaled, 
+                                                                  lowerRightYScaled));
                 }
             }
         });
