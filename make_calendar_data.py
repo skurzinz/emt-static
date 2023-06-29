993274f3bf6ac52546af438388464feb318e51de
csae8092 <peter.andorfer@oeaw.ac.at>
2023-06-29 11:10:50 +0200

    use dates values from correspaction sent #32

--- make_calendar_data.py
+++ make_calendar_data.py
@@ -1,3 +1,4 @@
+import csv
 import glob
 import os
 import json
@@ -6,23 +7,35 @@ from tqdm import tqdm
 files = sorted(glob.glob('./data/editions/*.xml'))
 out_file = "./html/js-data/calendarData.js"
 data = []
+no_dates = []
 for x in tqdm(files, total=len(files)):
     item = {}
     head, tail = os.path.split(x)
     doc = TeiReader(x)
     item['name'] = doc.any_xpath('//tei:title[@type="main"]/text()')[0]
+    sent_date_node = doc.any_xpath('//tei:correspAction/tei:date')[0]
+    date_vals = [value for key, value in sent_date_node.attrib.items()]
     try:
-        item['startDate'] = doc.any_xpath('//tei:origDate/@when-iso')[0]
-    except:
+        item['startDate'] = date_vals[0]
+    except IndexError:
+        no_dates.append(tail)
         continue
-    try:
-        item['tageszaehler'] = 1
-        item['id'] = tail.replace('.xml', '.html')
-        data.append(item)
-    except:
-        continue
+    item['tageszaehler'] = 1
+    item['id'] = tail.replace('.xml', '.html')
+    data.append(item)
+
+print(f"{len(data)} Datumsangaben aus {len(files)} extrahiert")
 
 print(f"writing calendar data to {out_file}")
 with open(out_file, 'w',  encoding='utf8') as f:
     my_js_variable = f"var calendarData = {json.dumps(data, ensure_ascii=False)}"
     f.write(my_js_variable)
+
+no_dates_file = './html/no_dates.csv'
+print(f"writing files without date to {no_dates_file}")
+
+with open(no_dates_file, "w", newline="") as csvfile:
+    my_writer = csv.writer(csvfile, delimiter=",")
+    my_writer.writerow(["file_name"])
+    for x in no_dates:
+        my_writer.writerow([x])
